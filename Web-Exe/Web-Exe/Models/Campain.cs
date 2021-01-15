1d112c51ff6ee45decbc896cb9e38640afcabd6e
Noamsachartov <>
2021-01-15 15:07:13 +0200

    Fix Get Campain

--- Web-Exe/Web-Exe/Models/Campain.cs
+++ Web-Exe/Web-Exe/Models/Campain.cs
@@ -5,9 +5,10 @@ using System.Web;
 
 namespace resturantwebApp.Models.DAL
 {
-    public class Campaign
+    public class Campain
     {
         int id;
+        string rest_name;
         int id_rest;
         int budget;
         double amount_use;
@@ -15,9 +16,10 @@ namespace resturantwebApp.Models.DAL
         int num_views;
         bool status;
 
-        public Campaign(int id, int id_rest, int budget, double amount_use, int num_clicks, int num_views, bool status)
+        public Campain(int id, string rest_name, int id_rest, int budget, double amount_use, int num_clicks, int num_views, bool status)
         {
             Id = id;
+            Rest_name = rest_name;
             Id_rest = id_rest;
             Budget = budget;
             Amount_use = amount_use;
@@ -27,6 +29,7 @@ namespace resturantwebApp.Models.DAL
         }
 
         public int Id { get => id; set => id = value; }
+        public string Rest_name { get => rest_name; set => rest_name = value; }
         public int Id_rest { get => id_rest; set => id_rest = value; }
         public int Budget { get => budget; set => budget = value; }
         public double Amount_use { get => amount_use; set => amount_use = value; }
@@ -34,13 +37,15 @@ namespace resturantwebApp.Models.DAL
         public int Num_views { get => num_views; set => num_views = value; }
         public bool Status { get => status; set => status = value; }
 
-        public Campaign() {}
 
+       
 
-        public List<Campaign> Read()
+        public Campain() { }
+
+        public List<Campain> Read()
         {
             DBServices dbs = new DBServices();
-            List<Campaign> campaignsList = dbs.getcampaingns();
+            List<Campain> campaignsList = dbs.getcampaingns();
             return campaignsList;
         }
 
