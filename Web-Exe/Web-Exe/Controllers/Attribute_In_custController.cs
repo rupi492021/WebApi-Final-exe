43bc74e5621d10d244a3c0785bfd425766689960
MaliItzhak <77485819+MaliItzhak@users.noreply.github.com>
2021-01-17 18:29:48 +0200

    new change

---
+++ Web-Exe/Web-Exe/Controllers/Attribute_In_custController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace cuisin.Models
+{
+    public class Attribute_In_custController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]Attribute_In_cust attribute_In_Cust)
+        {
+             attribute_In_Cust.Insert();
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
